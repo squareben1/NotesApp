0232090cf79446e8d0cd54ba419e6d820c67e5f9
Squareben1 <bengittins@live.com>
2020-02-03 18:25:18 +0000

    added methods to 1. return all note models and 2. create new ntoe and add to notes array

--- note-list-model.js
+++ note-list-model.js
@@ -3,8 +3,15 @@
     this.notes = []
   }
 
-  NoteList.prototype.add = function(note) {
+  NoteList.prototype.add = function(string) {
+    var note = new Note(string)
     this.notes.push(note)
+  }
+
+  NoteList.prototype.showNotes = function() {
+    for (var i = 0; i < this.notes.length; i++) {
+      return this.notes[i].text;
+    }
   }
 
   exports.NoteList = NoteList
--- spec/note-list-model-tests.js
+++ spec/note-list-model-tests.js
@@ -1,16 +1,26 @@
 (function testNoteList() {
   var noteList = new NoteList()
   assert.isTrue(noteList.notes.length === 0);
-  console.log('test passes')
+  console.log('testNoteList passing')
 })();
 
 (function testAddNoteList() {
   var notelist = new NoteList()
-  var noteOne = new Note('hello')
-  var noteTwo = new Note('test')
-  notelist.add(noteOne)
-  notelist.add(noteTwo)
+  // var noteOne = new Note('hello')
+  // var noteTwo = new Note('test')
+  notelist.add('hello')
+  notelist.add('test')
+  console.log(notelist.notes.text)
   assert.isTrue(notelist.notes.length === 2)
   assert.isTrue(notelist.notes[0].showNote() === 'hello')
-  console.log('test two')
+  console.log('addNoteList test passing')
+})();
+
+(function listNotes() {
+  var notelist = new NoteList()
+  
+  notelist.add('hello')
+  notelist.add('test')
+  assert.isTrue(notelist.showNotes() == 'hello', 'test')
+  console.log("listNotes test")
 })();
